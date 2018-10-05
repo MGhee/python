14ce54c102880ef54ea6e6a7426cf73a369a2b0c
MGhee <polironald@yahoo.com>
2018-10-05 20:16:23 +0300

    Good job

--- Hiihihi.py
+++ Hiihihi.py
@@ -1,2 +1,21 @@
-x=25
-print(x)
+print("enter number")
+x= input()
+print("second number")
+y= input()
+
+if x%2==0:
+     if y%2==0:
+        result = x*y
+        print(result)
+     else:
+        result= x**y
+        print(result)
+else:
+     if y%2==0:
+        result = x-y
+        print(result)
+     else:
+        result= y+x 
+        print(result)
+
+    
